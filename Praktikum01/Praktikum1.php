bda9a259abb25cd58c85a86b913cc9e8dbf2cf4e
Purbasari Suryaning Putri <71263332+purbasariputri@users.noreply.github.com>
2021-03-20 21:05:18 +0700

    Add files via upload

---
+++ Praktikum01/Praktikum1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Praktikum 1</title>
+</head>
+<body>
+	<center><h1>Selamat Datang di Website Saya!</h1>
+	<?php 
+		echo "<p>Purbasari Suryaning Putri</p>";
+		echo "<p>K3519068</p>";
+	?>
+	</center>
+</body>
+</html>
